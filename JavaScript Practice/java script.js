30519906c6147b5078da16372e8868a1491c9a9a
Sayed Mannan Ahmad <mannansarsobiz@gmail.com>
2020-06-29 16:55:11 +0530

    reading array and converting number to string.

--- JavaScript Practice/java script.js
+++ JavaScript Practice/java script.js
@@ -5,4 +5,15 @@ let firsrVar = 10;
 let secondVar = 15;
 console.log("First variable is = "+firsrVar+" And second variable is = "+secondVar);
 
+let myArray = [1, 2, 3, 4, 5];
+// document.write( myArray[0]);
+document.getElementById("para1").innerHTML = 
+"value at index 3 is = " + myArray[3]
+let indexValue = myArray[3];
+let numberToString = indexValue.toString();
+document.getElementById("para2").innerHTML = 
+"value at index 3 is converted to string and it is = " + numberToString+" which is now become a string.";
+
+
+
 
