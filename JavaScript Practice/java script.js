6b6a4ed503153f0732deb43636a98c2cef8875e7
Sayed Mannan Ahmad <mannansarsobiz@gmail.com>
2020-06-29 16:36:58 +0530

    Defines two variables and print them in the console.

--- JavaScript Practice/java script.js
+++ JavaScript Practice/java script.js
@@ -1,2 +1,8 @@
 let myVal = "Hello World !, Written by JavaScript.";
 document.getElementById("para").innerHTML = myVal;
+
+let firsrVar = 10;
+let secondVar = 15;
+console.log("First variable is = "+firsrVar+" And second variable is = "+secondVar);
+
+
