18c6814d95222f0fc7b93268d4c4ccf7596698ef
Sayed Mannan Ahmad <mannansarsobiz@gmail.com>
2020-06-29 17:20:33 +0530

    Created table with the help of html/css and find total and average of marks with the help of js.

--- JavaScript Practice/java script.js
+++ JavaScript Practice/java script.js
@@ -7,13 +7,20 @@ console.log("First variable is = "+firsrVar+" And second variable is = "+secondV
 
 let myArray = [1, 2, 3, 4, 5];
 // document.write( myArray[0]);
+
 document.getElementById("para1").innerHTML = 
 "value at index 3 is = " + myArray[3]
+
 let indexValue = myArray[3];
 let numberToString = indexValue.toString();
+
 document.getElementById("para2").innerHTML = 
 "value at index 3 is converted to string and it is = " + numberToString+" which is now become a string.";
 
+let totalMarks = 63 + 58 + 61 + 67 + 62;
+document.getElementById("total").innerHTML = " Total marks = " + totalMarks;
 
+let averageMarks = (63 + 58 + 61 + 67 + 62)/5;
+document.getElementById("average").innerHTML = " Average marks = " + averageMarks;
 
 
